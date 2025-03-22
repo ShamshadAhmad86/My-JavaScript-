fa1dbe90a600f17513b0f4f7b7a376f12554952e
ShamshadAhmad86 <shamshadakram315@gmail.com>
2025-03-22 23:59:04 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// Simple JavaScript Example
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("JavaScript File Loaded Successfully!");
+
+    // Example Function
+    function greetUser(name) {
+        return "Hello, " + name + "!";
+    }
+
+    // Testing Function
+    console.log(greetUser("GitHub User"));
+});
